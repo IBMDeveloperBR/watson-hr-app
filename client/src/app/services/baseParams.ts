d6de90552e035fdf2c7677c2dc18015ff2801e01
Pedro Henrique Lopes de Castro <pedrohlcastro@gmail.com>
2018-07-30 12:46:43 -0300

    [Update] Remove NLU Tab and Add Description

--- client/src/app/services/baseParams.ts
+++ client/src/app/services/baseParams.ts
@@ -9,11 +9,11 @@ export default {
         },
         {
           "name": "Escrupulosidade",
-          "min": 0.5
+          "min": 0.2
         },
         {
           "name": "Extroversão",
-          "min": 0.5
+          "min": 0.2
         },
         {
           "name": "Amabilidade",
@@ -27,65 +27,65 @@ export default {
       "needs": [
         {
         "name": "Desafio",
-        "min": 0.5
+        "min": 0.7
         },
         {
         "name": "Curiosidade",
-        "min": 0.5
+        "min": 0.7
         },
         {
         "name": "Empolgação",
-        "min": 0.5
+        "min": 0.7
         },
         {
         "name": "Harmonia",
-        "min": 0.5
+        "min": 0.7
         },
         {
         "name": "Ideal",
-        "min": 0.5
+        "min": 0.7
         },
         {
         "name": "Liberdade",
-        "min": 0.5
+        "min": 0.8
         },
         {
         "name": "Amor",
-        "min": 0.5
+        "min": 0.3
         },
         {
         "name": "Natureza prática",
-        "min": 0.5
+        "min": 0.8
         },
         {
         "name": "Expressão da própria personalidade",
-        "min": 0.5
+        "min": 0.3
         },
         {
         "name": "Estabilidade",
-        "min": 0.5
+        "min": 0.3
         }
       ],
       "values": [
         {
           "name": "Conservação",
-          "min": 0.5
+          "min": 0.3
         },
         {
           "name": "Abertura à mudança",
-          "min": 0.5
+          "min": 0.6
         },
         {
           "name": "Hedonismo",
-          "min": 0.5
+          "min": 0.1
         },
         {
           "name": "Autocrescimento",
-          "min": 0.5
+          "min": 0.7
         },
         {
           "name": "Autotranscendência",
-          "min": 0.5
+          "min": 0.2
         },
         
       ]
@@ -95,29 +95,29 @@ export default {
       "personality": [
         {
           "name": "Abertura",
-          "min": 0.5
+          "min": 0.7
         },
         {
           "name": "Escrupulosidade",
-          "min": 0.5
+          "min": 0.7
         },
         {
           "name": "Extroversão",
-          "min": 0.5
+          "min": 0.7
         },
         {
           "name": "Amabilidade",
-          "min": 0.5
+          "min": 0.7
         },
         {
           "name": "Faixa emocional",
-          "min": 0.5
+          "min": 0.2
         }
       ],
       "needs": [
         {
         "name": "Desafio",
-        "min": 0.5
+        "min": 0.7
         },
         {
         "name": "Curiosidade",
@@ -125,15 +125,15 @@ export default {
         },
         {
         "name": "Empolgação",
-        "min": 0.5
+        "min": 0.7
         },
         {
         "name": "Harmonia",
-        "min": 0.5
+        "min": 0.7
         },
         {
         "name": "Ideal",
-        "min": 0.5
+        "min": 0.4
         },
         {
         "name": "Liberdade",
@@ -145,15 +145,15 @@ export default {
         },
         {
         "name": "Natureza prática",
-        "min": 0.5
+        "min": 0.2
         },
         {
         "name": "Expressão da própria personalidade",
-        "min": 0.5
+        "min": 0.7
         },
         {
         "name": "Estabilidade",
-        "min": 0.5
+        "min": 0.2
         }
       ],
       "values": [
